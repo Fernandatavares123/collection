c7c5690c99b233962bdb98d3b0136bda9e54caec
Técnico em Informática <24121742018.1@senacrjedu.local>
2019-11-04 21:33:07 -0300

    2018.1

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -10,16 +10,10 @@ export class TabsPage {
 
 
   constructor(
+    
     protected usuarioService: UsuarioService
   ) 
-  {
-    this.usuarioService.getAll().subscribe(
-      res =>{
-        this.quantUsuario = res.length
-      }
-    );
-    }
-    
+ 
   }
 
 
