2e94cebcbd07cc67407afb3596bb45a6169bbc86
Técnico em Informática <24121742018.1@senacrjedu.local>
2019-11-04 21:14:54 -0300

    2018.1

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,14 +1,44 @@
 import { Injectable } from '@angular/core';
+
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth'
 import { Usuario } from '../modulo/usuario'
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  getAll(): any {
-    throw new Error("Method not implemented.");
+constructor(
+
+  protected fire: AngularFireDatabase,
+  protected afAuth: AngularFireAuth
+){}
+
+
+  save(usuario) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(usuario.email, usuario.pws)
+      .then(
+        res => {
+          usuario.email = null;
+          usuario.pws = null;
+          return this.fire.object("usuarios/" + res.user.uid).set(usuario);
+        },
+        erro =>{
+          console.log(erro);
+          this.afAuth.auth.currentUser.delete();
+        } 
+      )
   }
-  constructor(
   
-  ) { }
+  get(id) {
+    return this.fire.object<Usuario>("usuarios/" + id).valueChanges();
+  }
+
+  remover(id) {
+    return this.fire.object("usuarios/" + id).remove();
+  }
+
+  update(usuario, id) {
+    return this.fire.object("usuarios/" + id).update(usuario)
+  }
 }
