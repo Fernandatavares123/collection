b79920bfd76fa8f6477a7669c16e4aacef9e3263
Técnico em Informática <24121742018.1@senacrjedu.local>
2019-11-06 19:02:17 -0300

    atualizando

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFireAuth } from '@angular/fire/auth'
-import { Usuario } from '../modulo/usuario'
+import { Usuario } from '../model/usuario';
+
 
 @Injectable({
   providedIn: 'root'
